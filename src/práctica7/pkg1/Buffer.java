7b691b8fd2769ff57e67ce04f623aad8b4211149
Sergio <storres@cifpfbmoll.eu>
2020-03-29 19:33:37 +0200

    Primera expo

--- src/práctica7/pkg1/Buffer.java
+++ src/práctica7/pkg1/Buffer.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package práctica7.pkg1;
 
-/**
- *
- * @author Sergio
- */
 public class Buffer {
     
 }
